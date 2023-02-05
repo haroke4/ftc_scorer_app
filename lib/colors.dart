46d48de0084466b62e7af481538c454ae6ea5238
Alibi <alibiamanzolov@gmail.com>
2023-02-05 19:51:33 +0600

    Colors added

---
+++ lib/colors.dart
@@ -0,0 +1,10 @@
+// File for colors
+
+import 'dart:ui';
+
+Color darkGrey = const Color.fromRGBO(42, 43, 45, 1.0);
+Color regularGrey = const Color.fromRGBO(79, 79, 79, 1.0);
+Color lightGrey = const Color.fromRGBO(163, 163, 163, 1.0);
+Color regularRed = const Color.fromRGBO(234, 35, 42, 1.0);
+Color darkerRed = const Color.fromRGBO(143, 23, 28, 1.0);
+Color evenDarkerRed = const Color.fromRGBO(101, 15, 18, 1.0);
--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import '../colors.dart';
+
 class MainPage extends StatefulWidget {
   const MainPage({Key? key}) : super(key: key);
 
@@ -9,12 +11,34 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: darkGrey,
       body: SafeArea(
-        child: Text("nigger boi"),
+        child: Center(child: getMainWidget()),
       ),
+    );
+  }
+
+  Widget getMainWidget() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        getButton("Go to fucking jail", () {}),
+        const SizedBox(height: 10),
+        getButton("Select language", () {})
+      ],
+    );
+  }
+
+  Widget getButton(var text, var onPressed) {
+    return FloatingActionButton.extended(
+      onPressed: onPressed,
+      label: Text(text),
+      backgroundColor: regularRed,
     );
   }
 }
